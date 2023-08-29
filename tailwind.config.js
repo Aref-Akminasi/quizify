f23f1358f087b024dcce204964d795f8e2f147b2
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-08-29 14:03:08 +0200

    added file structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  purge: ['./index.html', './src/**/*.{js,ts}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
