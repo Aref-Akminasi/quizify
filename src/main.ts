3935a37ecb74748c105734b66fe206bb8b136c5a
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-08-30 23:00:40 +0200

    added fetching with axios

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,14 @@
 import './style.css';
 import './tailwind.css';
+import axios from 'axios';
+
+function getQuiz() {
+  axios
+    .get(
+      'https://raw.githubusercontent.com/Aref-Akminasi/quizify-json/main/quiz.json'
+    )
+    .then((res: any) => console.log(res.data.quiz[1]))
+    .catch((err: any) => console.log(err));
+}
+
+getQuiz();
