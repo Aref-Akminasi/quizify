258aed4383a942b900fca533145c3df990c2ed15
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-09-02 14:06:20 +0200

    built project

--- README.md
+++
@@ -1,2 +0,0 @@
-# quizify
- My project
--- src/darkmode.ts
+++
@@ -1,5 +0,0 @@
-const darkMode = document.getElementById('darkmode')!;
-
-darkMode.addEventListener('click', () => {
-  document.documentElement.classList.toggle('dark');
-});
